f0a7c4fc7b9908cb7130830fe82ae6e24fee92c6
workG <biryukov.gleb.98@mail.ru>
2022-08-14 21:54:21 +0300

    fix textarea form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -24,8 +24,8 @@
         <span class="label__text"> Описание товара </span>
       </label>
       <div class="form-group__input">
-        <input
-          class="form-group__control"
+        <textarea
+          class="form-group__control form-group__control_textarea"
           type="text"
           id="input_success"
           placeholder="Введите описание товара"
@@ -76,74 +76,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
 @import '@/assets/css/variables.scss';
 
 .form-group {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
 
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: 1rem;
+  &__label {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 13px;
+    letter-spacing: -0.02em;
 
-    &__label {
-        font-style: normal;
-        font-weight: 400;
-        font-size: 10px;
-        line-height: 13px;
-        letter-spacing: -0.02em;
+    color: $darkfiol;
+    position: relative;
+  }
 
-        color: $darkfiol;
-        position: relative;
-    }
+  &__icon {
+    display: inline;
+    width: 4px;
+    height: 4px;
+    position: absolute;
+  }
 
+  &__input {
+    display: inline-flex;
+    width: 100%;
+  }
 
+  &__input &__control {
+    background: $white;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
 
-    &__icon {
-        display: inline;
-        width: 4px;
-        height: 4px;
-        position: absolute;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 26px;
+    padding-top: 10px;
+    padding-left: 16px;
+    padding-bottom: 11px;
+    width: 100%;
+    outline: none;
+    border: none;
+    box-sizing: border-box;
+  }
 
-    }
-
-
-    &__input {
-        display: inline-flex;
-    }
-
-
-    &__input &__control {
-
-        background: $white;
-        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-        border-radius: 4px;
-
-        font-family: "Inter";
-        font-style: normal;
-        font-weight: 400;
-        font-size: 20px;
-        line-height: 26px;
-        padding-top: 10px;
-        padding-left: 16px;
-        padding-bottom: 11px;
-
-        width: 100%;
-        outline: none;
-        border: none;
-    }
-
+  &__control_textarea {
+    resize: none;
+    
+    overflow: hidden;
+  }
 }
 
 // /* .form-group .form-group__input .form-group__control:focus {
 //  border: 1px solid var(--blue-3);
 //  box-shadow: 0px 1px 1px rgba(51, 154, 240, 0.1);
 
-
 //     &_success .form-group__input .form-group__control:focus
 //         border: 1px solid var(--blue-3)
 //         box-shadow: 0px 1px 1px rgba(51, 154, 240, 0.1)
-
 
 //     &_invalid .form-group__input .form-group__control:focus
 //         border: 1px solid var(--red)
@@ -154,41 +149,37 @@ export default {
 /* button block */
 
 .button {
+  height: 36px;
+  border-radius: 10px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+  /* transition: opacity 200ms ease-in-out, background 200ms ease-in-out; */
+  padding: 10px 95px 11px 95px;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+  white-space: nowrap;
+  background: $lightGreen;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
 
-    height: 36px;
-    border-radius: 10px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 15px;
-    text-align: center;
-    /* transition: opacity 200ms ease-in-out, background 200ms ease-in-out; */
-    padding: 10px 95px 11px 95px;
-    border: none;
-    cursor: pointer;
-    display: inline-block;
-    white-space: nowrap;
-    background: $lightGreen;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
-
-
-    &:disabled {
-        color: #B4B4B4;
-        background: $white-disable;
-        pointer-events: none;
-        box-shadow: none;
-    }
+  &:disabled {
+    color: #b4b4b4;
+    background: $white-disable;
+    pointer-events: none;
+    box-shadow: none;
+  }
 }
 
 // /* primary block
 
-
 //     &_primary
 //         background-color: var(--primary-background-color)
 //         color: var(--primary-color)
-
 
 //     &_primary:hover
 //         background-color: var(--primary-hover)
@@ -198,7 +189,6 @@ export default {
 // /* .button_secondary {
 //  background-color: var(--secondary-background-color);
 //  color: var(--secondary-color);
-
 
 //     &_secondary:hover
 //         background-color: var(--secondary-hover)
@@ -210,7 +200,6 @@ export default {
 //  color: var(--alternate-color);
 //  box-sizing: border-box;
 //  border: 1px solid #ACB5BD;
-
 
 //     &_alternate:hover
 //         border: 1px solid #212429
